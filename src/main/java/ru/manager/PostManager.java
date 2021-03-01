ee754837deb6c3923b2a3bb89038ed103a3ac769
Расим <yamirasim@yandex.ru>
2021-03-01 22:27:54 +0300

    init3

---
+++ src/main/java/ru/manager/PostManager.java
@@ -0,0 +1,12 @@
+package ru.manager;
+
+import ru.domain.Post;
+
+public class PostManager {
+    public Post[] search(int ownerId, String domain, String query, boolean owners_only, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int post_id) {
+    }
+}
